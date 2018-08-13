ed76efd62d4b64b18e65d1ffe7370efd78195798
Albert Bos <albert@myndocs.nl>
2018-08-13 21:20:59 +0200

    Memory support fields redirectUris + oauthFlows

--- kotlin-oauth2-server-client-inmemory/src/main/java/nl/myndocs/oauth2/client/inmemory/ClientConfiguration.kt
+++ kotlin-oauth2-server-client-inmemory/src/main/java/nl/myndocs/oauth2/client/inmemory/ClientConfiguration.kt
@@ -3,5 +3,7 @@ package nl.myndocs.oauth2.client.inmemory
 data class ClientConfiguration(
         var clientId: String? = null,
         var clientSecret: String? = null,
-        var scopes: Set<String> = setOf()
+        var scopes: Set<String> = setOf(),
+        var redirectUris: Set<String> = setOf(),
+        var oauthFlows: Set<String> = setOf()
 )
