d258a7a3e60eccd68ffd2f6728c4e27eeec733b7
Albert Bos <albert@myndocs.nl>
2018-08-14 20:22:44 +0200

    In memory token store

---
+++ kotlin-oauth2-server-token-store-inmemory/src/main/java/nl/myndocs/oauth2/tokenstore/inmemory/InMemoryTokenStore.kt
@@ -0,0 +1,61 @@
+package nl.myndocs.oauth2.tokenstore.inmemory
+
+import nl.myndocs.oauth2.client.Client
+import nl.myndocs.oauth2.identity.Identity
+import nl.myndocs.oauth2.token.Token
+import nl.myndocs.oauth2.token.TokenStore
+import java.util.*
+
+class InMemoryTokenStore : TokenStore {
+    private val tokens = mutableMapOf<String, StoredToken>()
+    private val codes = mutableMapOf<String, StoredCode>()
+
+    override fun generateAndStoreTokenFor(identity: Identity, client: Client, requestedScopes: Set<String>): Token {
+        val token = Token(
+                UUID.randomUUID().toString(),
+                "bearer",
+                3600,
+                UUID.randomUUID().toString()
+        )
+
+        tokens[token.accessToken] = StoredToken(
+                token,
+                identity,
+                client,
+                requestedScopes
+        )
+
+        return token
+    }
+
+    override fun generateCodeTokenAndStoreFor(identity: Identity, client: Client, redirectUri: String, requestedScopes: Set<String>): String {
+        val code = UUID.randomUUID().toString()
+
+        codes[code] = StoredCode(
+                code,
+                identity,
+                client,
+                redirectUri,
+                requestedScopes
+        )
+
+        return code
+    }
+
+    fun userInfo(accessToken: String) = tokens[accessToken]!!
+
+    class StoredToken(
+            val token: Token,
+            val identity: Identity,
+            val client: Client,
+            val scopes: Set<String>
+    )
+
+    class StoredCode(
+            val code: String,
+            val identity: Identity,
+            val client: Client,
+            val redirectUri: String,
+            val scopes: Set<String>
+    )
+}
