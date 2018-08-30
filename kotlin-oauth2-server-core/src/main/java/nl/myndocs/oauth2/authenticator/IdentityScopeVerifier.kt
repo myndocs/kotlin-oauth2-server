609a35edcc07abdbe10fe1d1dcfbcedc4803b050
Albert <albert@myndocs.nl>
2018-08-30 07:03:16 +0200

    Improve validating scopes

--- kotlin-oauth2-server-core/src/main/java/nl/myndocs/oauth2/authenticator/IdentityScopeVerifier.kt
+++ kotlin-oauth2-server-core/src/main/java/nl/myndocs/oauth2/authenticator/IdentityScopeVerifier.kt
@@ -4,5 +4,8 @@ import nl.myndocs.oauth2.client.Client
 import nl.myndocs.oauth2.identity.Identity
 
 interface IdentityScopeVerifier {
-    fun validScopes(forClient: Client, identity: Identity, scopes: Set<String>): Boolean
+    /**
+     * Validate which scopes are allowed. Leave out the scopes which are not allowed
+     */
+    fun allowedScopes(forClient: Client, identity: Identity, scopes: Set<String>): Set<String>
 }
