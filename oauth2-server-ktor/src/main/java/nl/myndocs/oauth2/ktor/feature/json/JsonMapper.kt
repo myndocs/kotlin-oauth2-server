d9cadea49691ea510f65b86a955928f3256cdf94
Albert Bos <albert@myndocs.nl>
2018-08-30 19:31:58 +0200

    json module

--- oauth2-server-ktor/src/main/java/nl/myndocs/oauth2/ktor/feature/json/JsonMapper.kt
+++
@@ -1,9 +0,0 @@
-package nl.myndocs.oauth2.ktor.feature.json
-
-import com.google.gson.Gson
-
-object JsonMapper {
-    private val gson = Gson()
-
-    fun toJson(content: Any) = gson.toJson(content)
-}
