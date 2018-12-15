2b7df5fb8fa3480150aa83a66e5e740161d7b91b
A. Bos <albert@myndocs.nl>
2018-12-15 13:49:19 +0100

    Case insensitive headers (#39)
    * BasicAuthorizer headers should not be case sensitive
    
    * Add extension method for case insensitive headers key
    TokenInfo should get header case insensitive

--- oauth2-server-core/src/main/java/nl/myndocs/oauth2/request/auth/BasicAuthorizer.kt
+++ oauth2-server-core/src/main/java/nl/myndocs/oauth2/request/auth/BasicAuthorizer.kt
@@ -3,10 +3,13 @@ package nl.myndocs.oauth2.request.auth
 import nl.myndocs.oauth2.authenticator.Authorizer
 import nl.myndocs.oauth2.authenticator.Credentials
 import nl.myndocs.oauth2.request.CallContext
+import nl.myndocs.oauth2.request.headerCaseInsensitive
 
+// @TODO: BasicAuth should be injected instead of static call
 open class BasicAuthorizer(protected val context: CallContext) : Authorizer {
     override fun extractCredentials(): Credentials? {
-        val authorizationHeader = context.headers["authorization"] ?: ""
+        val authorizationHeader = context.headerCaseInsensitive("authorization") ?: ""
+
         return BasicAuth.parseCredentials(authorizationHeader)
     }
 
