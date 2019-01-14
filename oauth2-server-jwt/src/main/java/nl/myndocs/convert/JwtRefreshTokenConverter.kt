8d069603546df644316cc1c408791d5a5e1b4b67
Albert <albert@myndocs.nl>
2019-01-14 21:12:50 +0100

    Use interface instead of callback to have more control

--- oauth2-server-jwt/src/main/java/nl/myndocs/convert/JwtRefreshTokenConverter.kt
+++ oauth2-server-jwt/src/main/java/nl/myndocs/convert/JwtRefreshTokenConverter.kt
@@ -1,31 +1,22 @@
 package nl.myndocs.convert
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTCreator
 import com.auth0.jwt.algorithms.Algorithm
 import nl.myndocs.oauth2.token.RefreshToken
 import nl.myndocs.oauth2.token.converter.RefreshTokenConverter
 import java.time.Instant
-import java.util.*
 
 class JwtRefreshTokenConverter(
         private val algorithm: Algorithm,
         private val refreshTokenExpireInSeconds: Int = 86400,
-        private val jwtConfiguration: (JWTCreator.Builder) -> JWTCreator.Builder = { builder -> builder }
+        private val jwtBuilder: JwtBuilder = DefaultJwtBuilder
 ) : RefreshTokenConverter {
     override fun convertToToken(username: String?, clientId: String, requestedScopes: Set<String>): RefreshToken {
-        val jwtBuilder = JWT.create()
-                .withIssuedAt(Date.from(Instant.now()))
-                .withExpiresAt(
-                        Date.from(
-                                Instant.now()
-                                        .plusSeconds(refreshTokenExpireInSeconds.toLong())
-                        )
-                )
-                .withClaim("client_id", clientId)
-                .withArrayClaim("scopes", requestedScopes.toTypedArray())
-                .let { withBuilder -> if (username != null) withBuilder.withClaim("username", username) else withBuilder }
-                .let(jwtConfiguration)
+        val jwtBuilder = jwtBuilder.buildJwt(
+                username,
+                clientId,
+                requestedScopes,
+                refreshTokenExpireInSeconds.toLong()
+        )
 
         return RefreshToken(
                 jwtBuilder.sign(algorithm),
