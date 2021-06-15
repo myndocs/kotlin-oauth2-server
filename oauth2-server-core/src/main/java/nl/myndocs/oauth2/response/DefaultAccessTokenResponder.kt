1c9abfca2d519ca38420f7b762687715a3f82869
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-06-15 14:24:04 +0430

    Reformat DefaultAccessTokenResponder.kt

--- oauth2-server-core/src/main/java/nl/myndocs/oauth2/response/DefaultAccessTokenResponder.kt
+++ oauth2-server-core/src/main/java/nl/myndocs/oauth2/response/DefaultAccessTokenResponder.kt
@@ -3,13 +3,12 @@ package nl.myndocs.oauth2.response
 import nl.myndocs.oauth2.token.AccessToken
 
 object DefaultAccessTokenResponder : AccessTokenResponder {
-    override fun createResponse(accessToken: AccessToken): Map<String, Any?> =
-        with(accessToken) {
-            mapOf(
-                "access_token" to this.accessToken,
-                "token_type" to this.tokenType,
-                "expires_in" to this.expiresIn(),
-                "refresh_token" to this.refreshToken?.refreshToken
-            )
-        }
+    override fun createResponse(accessToken: AccessToken): Map<String, Any?> = with(accessToken) {
+        mapOf(
+            "access_token" to this.accessToken,
+            "token_type" to this.tokenType,
+            "expires_in" to this.expiresIn(),
+            "refresh_token" to this.refreshToken?.refreshToken
+        )
+    }
 }
